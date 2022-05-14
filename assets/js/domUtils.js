3e8773c9285afd43739016b341d5c484bc164187
SK <sarthakkarandikar03@gmail.com>
2022-05-14 17:40:50 +0530

    firebase integration

--- assets/js/domUtils.js
+++ assets/js/domUtils.js
@@ -1,3 +1,33 @@
+//Firebase Code
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-analytics.js";
+import { getDatabase, ref, push, set } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-database.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCRE1IqEmbBYEKm1viuilF1cTmaQYmt198",
+  authDomain: "passion8-app.firebaseapp.com",
+  databaseURL: "https://passion8-app-default-rtdb.firebaseio.com",
+  projectId: "passion8-app",
+  storageBucket: "passion8-app.appspot.com",
+  messagingSenderId: "727922558608",
+  appId: "1:727922558608:web:1cd131d0bd1da784b03a15",
+  measurementId: "G-CQYZJG76BN"
+};
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const database = getDatabase(app);
+
+//Page Functionality
+
 export function setupPageFunctionality() {
   document.getElementById('hamburger-close').onclick = overlayHandler.bind(this)
   document.getElementById('hamburger-open').onclick = overlayHandler.bind(this)
@@ -29,40 +59,14 @@ function overlayHandler() {
 
 //Email Input
 
-//Firebase Code
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-analytics.js";
-import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-database.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCRE1IqEmbBYEKm1viuilF1cTmaQYmt198",
-  authDomain: "passion8-app.firebaseapp.com",
-  databaseURL: "https://passion8-app-default-rtdb.firebaseio.com",
-  projectId: "passion8-app",
-  storageBucket: "passion8-app.appspot.com",
-  messagingSenderId: "727922558608",
-  appId: "1:727922558608:web:1cd131d0bd1da784b03a15",
-  measurementId: "G-CQYZJG76BN"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const database = getDatabase(app);
+const postListRef = ref(database, 'users');
+const newPostRef = push(postListRef);
 
 //Collect emails
 
-
 function collectUserEmail() {
-  var userid = Math.random();
   var email = document.getElementById("email");
-  set(ref(database, 'users/' + userid), {
+  set(newPostRef, {
     email: email,
   });
 }
