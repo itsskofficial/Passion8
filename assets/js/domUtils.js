8a362384758ec4f064504b58ef59e47d7e3b1a5a
SK <sarthakkarandikar03@gmail.com>
2022-05-14 17:15:29 +0530

    resolved breakpoints

--- assets/js/domUtils.js
+++ assets/js/domUtils.js
@@ -5,7 +5,10 @@ export function setupPageFunctionality() {
   document.getElementById('home').onclick = overlayHandler.bind(this)
   document.getElementById('privacy').onclick = overlayHandler.bind(this)
   document.getElementById('download').onclick = overlayHandler.bind(this)
+  document.getElementById('submit').onclick= collectUserEmail.bind(this)
 }
+
+//Navbar
 
 let toggleStatus = true
 
@@ -21,4 +24,45 @@ function overlayHandler() {
     overlayElement.classList.add('passion__navbar-mobileScreen-overlayClose')
     toggleStatus = true
   }
+}
+
+
+//Email Input
+
+//Firebase Code
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-analytics.js";
+import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-database.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCRE1IqEmbBYEKm1viuilF1cTmaQYmt198",
+  authDomain: "passion8-app.firebaseapp.com",
+  databaseURL: "https://passion8-app-default-rtdb.firebaseio.com",
+  projectId: "passion8-app",
+  storageBucket: "passion8-app.appspot.com",
+  messagingSenderId: "727922558608",
+  appId: "1:727922558608:web:1cd131d0bd1da784b03a15",
+  measurementId: "G-CQYZJG76BN"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const database = getDatabase(app);
+
+//Collect emails
+
+
+function collectUserEmail() {
+  var userid = Math.random();
+  var email = document.getElementById("email");
+  set(ref(database, 'users/' + userid), {
+    email: email,
+  });
 }
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,6 @@
 import { setupPageFunctionality } from './domUtils.js'
-import { getDatabase, ref, set } from "firebase/database";
 
 setupPageFunctionality()
 
-function writeUserData() {
-    const db = getDatabase();
-    var email=document.getElementById("fname").value
-  set(ref(db, 'users/'), {
-    email: email,
-  });
-}
 
 
