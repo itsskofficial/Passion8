57ea30774423f086b766632d9f3c8331aa95c016
SK <sarthakkarandikar03@gmail.com>
2022-05-14 13:16:59 +0530

    configuring firebase

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,8 +3,9 @@ import { getDatabase, ref, set } from "firebase/database";
 
 setupPageFunctionality()
 
-function writeUserData(email) {
-  const db = getDatabase();
+function writeUserData() {
+    const db = getDatabase();
+    var email=document.getElementById("fname").value
   set(ref(db, 'users/'), {
     email: email,
   });
