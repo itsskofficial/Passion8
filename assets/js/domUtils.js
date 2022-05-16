564ad344f216a32e3c4f6bf5bbbd38f1e7c95b69
SK <sarthakkarandikar03@gmail.com>
2022-05-16 17:35:10 +0530

    resolved hamburger menu

--- assets/js/domUtils.js
+++ assets/js/domUtils.js
@@ -1,14 +1,17 @@
 //Firebase Code
 
 // Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getDatabase, ref, set } from "firebase/database";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
   apiKey: "AIzaSyCRE1IqEmbBYEKm1viuilF1cTmaQYmt198",
   authDomain: "passion8-app.firebaseapp.com",
